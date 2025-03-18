484ed6c993f1b3ec22106b21062aba960e6e1965
Pranay Thapa <47719584+pranaythapa@users.noreply.github.com>
2025-03-18 17:58:56 -0500

    Update VirtualIO.java

--- lab04/VirtualIO.java
+++ lab04/VirtualIO.java
@@ -1,37 +1,61 @@
 package lab04;
-
 import java.util.*;
 
 public class VirtualIO {
     private Queue<PCB> ioQueue; // FIFO holder for io opperations
     private PCB currentProcess; // holds the current process being run
-    public int remainingIOTime; // holds the remaining time for io operations. may need to be private idk
+    private Queue<PCB> finishedQueue;
+    //private int remainingIOTime; // holds the remaining time for io operations. may need to be public idk
 
     public VirtualIO() {
         this.ioQueue = new LinkedList<>();
         this.currentProcess = null;
-        this.remainingIOTime = 0;
+        this.finishedQueue = new LinkedList<>();
+        //this.remainingIOTime = 0;
     }
 
-    public void addProcess(PCB process, int time) { //adding procsess to the queue. starts one if the idle
+    public Queue<PCB> getQueue(){
+        return ioQueue;
+    }
+
+    public void addProcess(PCB process) { //adding procsess to the queue. starts one if the idle
         process.setWaitingTime(0);
         ioQueue.add(process);
+        //this.remainingIOTime = time;
         if (currentProcess == null)
             startNew();
     }
 
+    public PCB getProcess() {
+        return ioQueue.peek();
+    }
+
+    public PCB getReadyProcess(){
+        PCB temp = finishedQueue.peek();
+        finishedQueue.remove();
+        return temp;
+    }
+
+    public boolean availableProcs(){
+        return finishedQueue.size() > 0;
+    }
+
     private void startNew() { //starting a new process if there is one avalible
         if (!ioQueue.isEmpty()) {
-            currentProcess = ioQueue.poll();
-            remainingIOTime = currentProcess.getCpuBurst()[0]; //set burst time.
+            //currentProcess = ioQueue.poll();
+            currentProcess = ioQueue.peek();
+            //remainingIOTime = currentProcess.getCpuBurst()[0]; //set burst time.
         }
     }
 
     public void executeIO(){ // executes the io operation
         if(currentProcess != null){
-            remainingIOTime--;
-            if(remainingIOTime == 0){
-                currentProcess = null;
+            currentProcess.setBurst(currentProcess.getBurst() - 1);
+            if(currentProcess.getBurst() == 0){
+                //currentProcess = null;
+                currentProcess.setIndex(currentProcess.getIndex() + 1);
+                finishedQueue.add(currentProcess);
+                ioQueue.remove(currentProcess);
                 startNew();
             }
         }
@@ -40,4 +64,5 @@ public class VirtualIO {
     public boolean isIdle(){ //public getter for status
         return currentProcess == null;
     }
+
 }
